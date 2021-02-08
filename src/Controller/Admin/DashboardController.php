63f7a3dfb622c2e4cf329a0d93c1c05eafd19e7d
François <contact@cyannlab.com>
2021-02-08 15:39:04 +0100

    Mise en place criptage MDP dans easyadmin. Mise en place de la connexion.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Categorie;
 use App\Entity\HeroHeader;
 use App\Entity\Page;
+use App\Entity\User;
 use App\Entity\Work;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -35,5 +36,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Headers', 'far fa-window-maximize', HeroHeader::class);
         yield MenuItem::linkToCrud('Projets', 'fas fa-desktop', Work::class);
         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Categorie::class);
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
     }
 }
