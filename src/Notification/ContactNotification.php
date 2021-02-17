a7c634a30545f67f9b7371a511cba28c688ae16a
François <contact@cyannlab.com>
2021-02-17 11:11:45 +0100

    Refactorisation aderesse email admin en paramètre

--- src/Notification/ContactNotification.php
+++ src/Notification/ContactNotification.php
@@ -3,6 +3,7 @@
 namespace App\Notification;
 
 use App\Entity\Contact;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Twig\Environment;
 
 class ContactNotification
@@ -17,17 +18,26 @@ class ContactNotification
      */
     private $renderer;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $renderer)
+    /**
+     * @param ParameterBagInterface 
+     */
+    private $params;
+
+
+    public function __construct(\Swift_Mailer $mailer, Environment $renderer, ParameterBagInterface $params)
     {
         $this->mailer = $mailer;
         $this->renderer = $renderer;
+        $this->params = $params;
     }
 
     public function notify(Contact $contact)
     {
+        $adminMail = $this->params->get('admin_mail');
+
         $message = (new \Swift_Message('Message de cyannlab.com'))
             ->setFrom($contact->getEmail())
-            ->setTo('contact@cyannlab.com')
+            ->setTo($adminMail)
             ->setReplyTo($contact->getEmail())
             ->setBody($this->renderer->render('emails/contact.html.twig', [
                 'contact' => $contact
